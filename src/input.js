2090b239b47879f873890452094153f394ce08e5
Daniel Buchner <danieljb2@gmail.com>
2015-04-22 21:23:58 -0700

    tap event for empty clicks

--- src/input.js
+++ src/input.js
@@ -39,6 +39,9 @@
       }
     },
     events:{
+      tap: function(e){
+        if (e.target == this) this.focus();
+      },
       'focus:delegate(.x-input-text input)': function(e){
         e.currentTarget.setAttribute('focus', '');
       },
